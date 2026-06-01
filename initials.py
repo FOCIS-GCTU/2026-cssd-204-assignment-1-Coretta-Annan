222954db9be39330f1fe0a105a794be925ca7236
Coretta-Annan <2425401321@live.gctu.edu.gh>
2026-06-01 19:41:15 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my
+# Assignment Number: 1
+#
+# Name: Coretta Araba Annan
+# STUDENT ID:  2425401321
+# Email: 2425401321@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, I Coretta Araba Annan, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials (small) and then in large block letters made of the initial letters and periods
 
+    print()
+    print("...CAA")
+    print()
+    print("....CCCCCCCCCCC.........AAAAAAAAAA..........AAAAAAAAAA......")
+    print("...CC..................AA........AA........AA........AA.....")
+    print("...CC..................AA........AA........AA........AA.....")
+    print("...CC..................AA........AA........AA........AA.....")
+    print("...CC..................AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...CC..................AAAAAAAAAAAA........AAAAAAAAAAAA.....")
+    print("...CC..................AA........AA........AA........AA.....")
+    print("...CC..................AA........AA........AA........AA.....")
+    print("...CC.............**...AA........AA...**...AA........AA...**")
+    print("....CCCCCCCCCCC...**...AA........AA...**...AA........AA...**")
+    print()
 
 
 main()
